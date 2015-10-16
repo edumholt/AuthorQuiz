02363e12228542a267bd924ee879764cbad8fb8f
Edward Dumholt <edumholt@gmail.com>
2015-10-16 13:53:14 -0500

    started app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  var Quiz = React.createClass({
+    render: function() {
+      return <div>test</div>;
+    }
+  });
+
+  React.render(
+    <Quiz data={"foo"} />,
+      document.getElementById('app');
+  )
+
+})();
